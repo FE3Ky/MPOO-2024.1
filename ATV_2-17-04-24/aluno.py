ec33c3ce2ba5bde5e4ff6ec5f2f917dee0087ca8
Felipe <fe3ky07@gmail.com>
2024-04-13 21:04:35 -0300

    Adicionada Segunda Atividade

---
+++ ATV_2-17-04-24/aluno.py
@@ -0,0 +1,9 @@
+class Aluno:
+    def __init__(self, nome, cpf, matricula) -> None:
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__matricula = matricula
+    
+    def definirNome():
+        if cpf:
+            nome = input("Nome: ")
